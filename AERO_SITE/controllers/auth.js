4cb84061f40966cc21c586f3bfd48e8dd55560c8
Your Name <sahilkumar23686@gmail.com>
2024-09-13 00:04:19 +0530

    first commit

---
+++ AERO_SITE/controllers/auth.js
@@ -0,0 +1,60 @@
+const _ = require("lodash");
+const User = require("../models/user");
+
+// render the register page
+module.exports.renderRegister = (req, res) => {
+  res.render("auth/register");
+};
+
+// store user's data in DB and login
+module.exports.register = async (req, res, next) => {
+  try {
+    let { name, username, password } = req.body;
+    name = _.startCase(_.camelCase(name));
+    const user = new User({ name, username });
+    const registeredUser = await User.register(user, password);
+    req.login(registeredUser, (err) => {
+      if (err) return next(err);
+      res.redirect("/");
+    });
+  } catch (e) {
+    req.flash("error", "Enter the details correctly!");
+    res.redirect("/register");
+  }
+};
+
+// render the login page
+module.exports.renderLogin = (req, res) => {
+  res.render("auth/login");
+};
+
+module.exports.loginAdmin = (req, res) => {
+   let{username,password} = req.body;
+   if(username=="admin"&&password=="12345"){
+    res.redirect("/admin");
+   }
+  req.flash("success", "Logged in successfully!");
+
+  res.redirect(redirectUrl);
+};
+
+
+// check login details - authentication
+module.exports.login = (req, res) => {
+  const redirectUrl = req.session.returnTo || "/";
+  req.flash("success", "Logged in successfully!");
+
+  res.redirect(redirectUrl);
+};
+
+// logout user
+module.exports.logout = (req, res) => {
+  req.logout(function (err) {
+    req.flash("success", "Logged out successfully!");
+
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+};
